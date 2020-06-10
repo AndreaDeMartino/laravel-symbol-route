7e193c5b40ffa9eaa6767a8ee43c0cf3d299ffdb
Andrea De Martino <ademartino1992@gmail.com>
2020-06-10 17:50:03 +0200

    Minor Fixing

--- resources/views/info/index.blade.php
+++ resources/views/info/index.blade.php
@@ -6,14 +6,13 @@
     <div class="container">
       <div class="row">
 
-        {{-- Create all buttons with label, using $buttons array --}}
+        {{-- Create all buttons with label, using array $buttons --}}
         @foreach($buttons as $button)
         <a href="info" class="btn btn-secondary col-5 m-3">{{ $button }}</a>
         @endforeach
 
       </div>
     </div>
-
   </main>
 
 @endsection
